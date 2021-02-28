b41e0ce52bc77f0492795b79686e665f318abec7
Yudai Hayashi <yudai0913haya@gmail.com>
2021-02-28 16:45:34 +0900

    Implemented KK transform from real to imag

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,32 @@ use pyo3::wrap_pyfunction;
 
 #[pymodule]
 fn pykk(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(get_zeros_array))?;
+    m.add_wrapped(wrap_pyfunction!(real2imag))?;
     Ok(())
 }
 
 #[pyfunction]
-fn get_zeros_array(n: Vec<i32>) -> PyResult<Vec<i32>> {
-    Ok(get_ones(n))
+fn real2imag(x: Vec<f64>, y: Vec<f64>) -> PyResult<Vec<f64>> {
+    const PI: f64 = 3.141592653589;
+    let mut result = vec![0.0; y.len()];
+
+    for i in 0..x.len() {
+        result[i] = -2.0 / PI * integrate(&x, &y, i);
+    }
+
+    Ok(result)
 }
 
-fn get_ones(n: Vec<i32>) -> Vec<i32> {
-    vec![1; n.len()]
+fn integrate(x: &Vec<f64>, y: &Vec<f64>, num: usize) -> f64{
+    let mut result = 0.0;
+    let diff = x[1] - x[0];
+
+    for i in 0..x.len() {
+        if i == num {
+            continue;
+        }
+        result += x[num] * y[i] / (x[i]*x[i] - x[num]*x[num]) * diff;
+    }
+
+    result
 }
